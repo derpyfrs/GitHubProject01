c3f8131e2eb43a439570a7cee02b4ed2e158dffe
derpyfrs <simon_chanct@hotmail.com>
2017-08-01 04:15:26 +0000

    project has finished

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,8 @@
 var request = require('request');
 var env = require('dotenv').config();
 var apiRoot = "https://" + process.env.GITHUB_USER+ ':' + process.env.GITHUB_TOKEN + '@api.github.com'
+var fs = require('fs');
+var userInput = process.argv.slice(2);
 
 // console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -18,13 +20,20 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 
-
-getRepoContributors("jquery", "jquery", function(err, result) {
+if(userInput.length !== 2){
+  console.log('Incorrect input, please provide Repo Owner and Repo Name');
+  return;
+}
+getRepoContributors(userInput[0], userInput[1], function(err, result) {
   console.log("Errors:", err);
 
   result.forEach(function(user){
-    console.log(user.avatar_url)
+    var filePath = './' + user.login + '.jpeg';
+    console.log(user.avatar_url);
+    downloadImageByURL(user.avatar_url, filePath);
   });
 });
 
-
+function downloadImageByURL(url, filePath) {
+  request.get(url).pipe(fs.createWriteStream(filePath));
+}
