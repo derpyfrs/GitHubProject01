fe68ee3dd2e89acaec5e88be5c5ff79e5abe2995
derpyfrs <simon_chanct@hotmail.com>
2017-08-01 03:36:37 +0000

    updating api log within implement git repo stage

--- download_avatars.js
+++ download_avatars.js
@@ -1,1 +1,30 @@
-download_avatars.js
+var request = require('request');
+var env = require('dotenv').config();
+var apiRoot = "https://" + process.env.GITHUB_USER+ ':' + process.env.GITHUB_TOKEN + '@api.github.com'
+
+// console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+ request.get({
+  url: apiRoot + "/repos/" + repoOwner  + "/" + repoName + "/contributors",
+  headers: {
+      'User-Agent': 'derpyfrs'
+    }
+  },
+  function(err, response, body) {
+    cb(err, JSON.parse(body));
+  });
+}
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+
+  result.forEach(function(user){
+    console.log(user.avatar_url)
+  });
+});
+
+
